f447ec3e783d3391e0abcd2183c2395f35d6187d
Saisudarshan2006 <saisudarshan2006@gmail.com>
2026-02-19 15:37:52 +0530

    Add files via upload

---
+++ script6.js
@@ -0,0 +1,19 @@
+const filterBtns = document.querySelectorAll(".filter-btn");
+const galleryItems = document.querySelectorAll(".gallery-item");
+
+filterBtns.forEach(btn => {
+  btn.addEventListener("click", () => {
+    filterBtns.forEach(b => b.classList.remove("active"));
+    btn.classList.add("active");
+
+    const filter = btn.getAttribute("data-filter");
+
+    galleryItems.forEach(item => {
+      if (filter === "all" || item.classList.contains(filter)) {
+        item.style.display = "block";
+      } else {
+        item.style.display = "none";
+      }
+    });
+  });
+});
